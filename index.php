2c1e70d995cf3dc1d872a79a5daf95b37616b62e
DKoTechnology <2733392694@qq.com>
2024-08-21 19:06:19 +0800

    Create base config

---
+++ index.php
@@ -0,0 +1,24 @@
+<?php
+/*
+AnyVICP 2
+By DKoTechnology
+
+Index page
+*/
+?>
+<!doctype html>
+<html lang="zh-CN">
+  <head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1, shrink-to-fit=no"/>
+    <meta name="renderer" content="webkit"/>
+
+    <link rel="stylesheet" href="https://unpkg.com/mdui@2/mdui.css">
+    <script src="https://unpkg.com/mdui@2/mdui.global.js"></script>
+    <script src="https://cdn.bootcdn.net/ajax/libs/jquery/3.6.0/jquery.min.js"></script>
+    <title></title>
+  </head>
+  <body>
+    
+  </body>
+</html>
